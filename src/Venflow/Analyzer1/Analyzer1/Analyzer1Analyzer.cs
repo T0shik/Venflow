d1ace32070035f975349c0c6fa7ed8c24620a935
Twenty <twenty@translucent.at>
2021-01-06 16:08:07 +0100

    Added base analyzer

---
+++ src/Venflow/Analyzer1/Analyzer1/Analyzer1Analyzer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Analyzer1
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class Analyzer1Analyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "Venflow.Interpolation";
+
+        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+        // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Naming";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.InvocationExpression);
+        }
+
+        private static void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var node = (InvocationExpressionSyntax)context.Node;
+
+            if (!(node.Expression is MemberAccessExpressionSyntax methodCallExpression) ||
+                node.ArgumentList.Arguments.Count == 0)
+                return;
+
+            var names = methodCallExpression.ChildNodes().OfType<IdentifierNameSyntax>().Take(2).ToArray();
+
+            if (names.Length != 2)
+                return;
+
+            if (names[1].Identifier.Text == "WriteLine")
+                return;
+
+            if (!(node.ArgumentList.Arguments.First().Expression is InterpolatedStringExpressionSyntax))
+                return;
+
+            var type = context.SemanticModel.GetTypeInfo(names[0]);
+
+            var fullTypeName = type.Type.ContainingNamespace + "." + type.Type.Name;
+
+            if (fullTypeName != "System.Console")
+                return;
+
+            context.ReportDiagnostic(Diagnostic.Create(Rule, node.GetLocation()));
+
+        }
+    }
+}
